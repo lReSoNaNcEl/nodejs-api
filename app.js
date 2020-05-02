e0548bc3c4d037a11298c0cb0d3383c706793058
Ivan <iresonancei@mail.com>
2020-05-02 14:38:46 +0300

    'push'

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = require('express')()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-const cors = require('cors')
+// const cors = require('cors')
 const mongoose = require('mongoose')
 const passport = require('passport')
 
@@ -17,12 +17,17 @@ require('./middleware/passport')(passport)
 
 app.use('/photos', express.static('photos'))
 app.use(morgan('dev'))
-app.use(cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 200
-}))
+// app.use(cors({
+//     origin: "*",
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     preflightContinue: false,
+//     optionsSuccessStatus: 200
+// }))
+app.all((req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, HEAD");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+});
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
